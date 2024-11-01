32eeca4dec50b4855b542bdf7a287872c87acf01
mackenna <mackenna.rines@kearsarge.org>
2024-11-01 09:03:02 -0400

    continued writing story 2

--- Story2.py
+++ Story2.py
@@ -4,10 +4,13 @@ def Story2(debug=False):
     if debug: print("Story1 Function")
     
     print("\n")
+    friendname1 = getword ('Enter a name: ', debug)
+    song1 = getsong ('Enter a song: ', debug)
     
     
     out = "\n"
-    out = 'one day me and my friend'
-    out = 'were listening to a song called'
+    out = 'one day me and my friend ' + friendname1
+    out = 'were listening to a song called ' + song1
+    out = 'and while we where listening to ' + song1 ' when suddenly'
     
     return out
