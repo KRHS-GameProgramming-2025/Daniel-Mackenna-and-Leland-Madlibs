04020bfa92ee988b4745b74968eacb6f18ff2ccf
mackenna <mackenna.rines@kearsarge.org>
2024-11-05 09:03:51 -0500

    continued writing story 2

--- Story2.py
+++ Story2.py
@@ -6,8 +6,8 @@ def Story2(debug=False):
     print("\n")
     friendname1 = getword ('Enter a name: ', debug)
     song1 = getsong ('Enter a song: ', debug)
-    animal1 = getanimal('Enter a animal: ', debug)
-    action
+    animal1 = getanimal1('Enter a animal: ', debug)
+    action = getaction1("Enter a action word", dubug)
     
     
     
@@ -17,6 +17,6 @@ def Story2(debug=False):
     out += " and while we where listening to " + song1 
     out += " suddenly " +  animal1
     out += " jumped out of no where "
-    out += "then we decided to "
+    out += "then we decided to " + action
     
     return out
