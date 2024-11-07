d93657d6c6c9ffe83aab12b3cd1d9f0e444b86e4
leland <leland.cobb@kearsarge.org>
2024-11-07 08:29:57 -0500

    added more story to story 3

--- Story3.py
+++ Story3.py
@@ -5,11 +5,16 @@ def Story3(debug=True):
     
     print("\n")
     friendname1 = getword ("Enter a name: ", debug)
+    item1 = getword ("Enter a Item used for diging: ", debug)
+    animal1 = getAnimal ("Enter a scary animal: ", debug)
     
     
     out = "\n"
-    out += "one day me and my friend, " + friendname1
-    out += "grabed a, "
+    out += " one day me and my friend, " + friendname1
+    out += " wanted to dig a hole to get to the other side of the earth s0 we grabed a, " + item1 + " from the shed as we grabed the, " + item1 + " a, " + animal1 + " appered out of nowhere, "
+    out += " me and my friend ran because of the scary, " + animal1
+    out += " after me and my friend calmed down we started diging, "
+    out += " soon we hit a big rock so we had to get a new tool from the shed with the scary, " + animal1
     
     return out
 
