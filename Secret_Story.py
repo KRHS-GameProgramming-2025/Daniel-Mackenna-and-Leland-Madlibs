e55a2fddabf93a32df851dd7c754b02000136295
leland <leland.cobb@kearsarge.org>
2024-11-07 09:20:20 -0500

    made new secret story file delete old file also update getters and madlibs so option 6/secret story works

---
+++ Secret_Story.py
@@ -0,0 +1,13 @@
+from Getters import *
+
+def Secret_Story(debug=True):
+    if debug: print("Story3 Function")
+    
+    print("\n")
+    
+    
+    out = "\n"
+    out += " hi "
+
+    
+    return out
