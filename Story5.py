bf00465bca425c7955911dd8ad7ad4ccf708c96b
mackenna <mackenna.rines@kearsarge.org>
2024-11-12 09:09:55 -0500

    started writing story 5

--- Story5.py
+++ Story5.py
@@ -2,3 +2,11 @@ from Getters import *
 # Easter egg # = 8
 def Story5(debug=False):
     if debug: print("Story5 Function")
+    
+    print("\n")
+    
+    
+    
+    
+    out = "\n"
+    out += " one day I decided to organize a shop so i decided to start with " + shopitem1
